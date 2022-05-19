c7ceaa6d186def929be79dfaa602a2f145582699
liyansum <103241475+liyansum@users.noreply.github.com>
2022-05-19 22:11:43 +0800

    Update api_web.py

--- api_web.py
+++ api_web.py
@@ -147,6 +147,10 @@ banner_img: https://ok.166.net/reunionpub/ds/kol/20210722/001554-k2u90bj7ay.png?
         # 在 reademe.md 中写文件
         with open("README.md", "w", encoding="utf8") as mm:
             mm.write(md)
+            
+        # 输出网页
+        with open("index.html", "w", encoding="utf8") as h:
+            h.write(html)
 
         log.logger.info(f"{file_name} 保存成功!")
         return os.path.join("docs", "%s.md" % (file_name)), html
